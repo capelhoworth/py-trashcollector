fd1cdeebb34be64015fdaaf7c509e8618f35b1bc
CapelHoworth <capelhoworth@gmail.com>
2023-10-04 20:12:51 -0400

    added background, player icon, members images

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
 import sys
 
+# Initialize pygame
 pygame.init
 
+# Window Display
 pygame.display.set_caption('Trash Signal Collecor')
 
-screen = pygame.display.set_mode((640, 480))
+# Create Screen
+screen = pygame.display.set_mode((940, 780))
+
+# Background
+background = pygame.image.load("background.png")
 
 clock = pygame.time.Clock()
 
-while True:
+# Game Loop
+running = True
+while running:
+    # Background Image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit(
